ae0e1721fe3cb7eb4050d32106c7e74bbe8f1f5b
Shanderson Vieira <shanderson.vieira@hotmail.com>
2025-09-13 23:48:40 -0300

    feat: setting pipeline

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "bucket-backend" {
-  bucket = var.bucket_name
-  tags   = var.tags
-}
+# resource "aws_s3_bucket" "bucket-backend" {
+#   bucket = var.bucket_name
+#   tags   = var.tags
+# }
 
 resource "aws_s3_bucket_ownership_controls" "this" {
   bucket = aws_s3_bucket.bucket-backend.id
